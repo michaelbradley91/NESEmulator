62e7864a4850851d06ac294b383f554707afb90b
Michael Bradley <michael.bradley@hotmail.co.uk>
2018-06-04 22:29:36 +0100

    Initialising interrupt flag to 1

--- NESEmulator.CPU/ProcessorStatus.cs
+++ NESEmulator.CPU/ProcessorStatus.cs
@@ -9,7 +9,9 @@
         public bool ZeroResult { get; set; }
 
         // I = bit 2
-        public bool InterruptDisable { get; set; }
+        // Initialised to 1. See https://stackoverflow.com/questions/16913423/why-is-the-initial-state-of-the-interrupt-flag-of-the-6502-a-1
+        // for an explanation.
+        public bool InterruptDisable { get; set; } = true;
 
         // D = bit 3
         public bool DecimalMode { get; set; }
@@ -19,6 +21,7 @@
 
         // bit 5 = expansion - no guarantees on its value.
         // It was going to be used in a future expansion of the micro processor
+        // It is initialised to 1 in the 6502.
 
         // V = bit 6
         public bool Overflow { get; set; }
